c5b850f44535efe5ab33cdbb626440b8c86edea2
James Kominick <james@kominick.com>
2021-06-25 21:51:20 +0000

    make deploy script better

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,93 @@
-set -ex
+set -e
 
+RENEW=false
+LB=false
+TAIL=false
+ALL=false
+ALL_SVCS=false
+SVC=
 
-if [[ "$1" = "renew" ]]; then
-    ./certs/make-cert.sh renew
+function die_usage {
+  echo "Usage: $(basename $0) [-hnapr] [-s service]"
+  echo ""
+  echo "Deploy stuff"
+  echo ""
+  echo "Options:"
+  echo "  -h           Show this beautiful message"
+  echo "  -r           Run cert renewal script"
+  echo "  -n           Deploy the nginx configs and restart nginx"
+  echo "  -a           Deploy _everything_"
+  echo "  -p           Deploy all services"
+  echo "  -f           Tail nginx logs after running"
+  echo "  -s SERVICE   Deploy a specific service"
+  exit 1
+}
+
+while getopts "hrnapfs:" opt; do
+  case $opt in
+    h) die_usage ;;
+    r) RENEW=true ;;
+    n) LB=true ;;
+    f) TAIL=true ;;
+    a) ALL=true ;;
+    p) ALL_SVCS=true ;;
+    s) SVC="$OPTARG" ;;
+    \? ) die_usage
+      ;;
+  esac
+done
+
+echo "ARGS:"
+echo "  RENEW=$RENEW, LB=$LB"
+echo "  ALL=$ALL, ALL_SVCS=$ALL_SVCS"
+echo "  SVC=$SVC"
+echo ""
+
+if $RENEW; then
+    echo "*** renewing certs..."
+    set -x
+    ./certs/make-all.sh renew
+    set +x
 fi
 
-sudo cp nginx/404.html /usr/share/nginx/html/404.html
-sudo cp nginx/kom-ssl-config.conf /etc/nginx/snippets/kom-ssl-config.conf
-sudo cp nginx/kom.conf /etc/nginx/sites-available/kom.conf
-sudo ln -sf /etc/nginx/sites-available/kom.conf /etc/nginx/sites-enabled/kom.conf
+if $ALL || $LB; then
+    echo "*** deploying nginx..."
+    set -x
+    sudo cp nginx/404.html /usr/share/nginx/html/404.html
+    sudo cp nginx/kom-ssl-config.conf /etc/nginx/snippets/kom-ssl-config.conf
+    sudo cp nginx/kom-org-ssl-config.conf /etc/nginx/snippets/kom-org-ssl-config.conf
+    sudo cp nginx/kom-dev-ssl-config.conf /etc/nginx/snippets/kom-dev-ssl-config.conf
+    sudo cp nginx/kom.conf /etc/nginx/sites-available/kom.conf
+    sudo ln -sf /etc/nginx/sites-available/kom.conf /etc/nginx/sites-enabled/kom.conf
 
-sudo nginx -t
-sudo systemctl restart nginx
+    sudo nginx -t
+    sudo systemctl restart nginx
+    set +x
+fi
 
-sudo cp systemd/upaste.service /etc/systemd/system/upaste.service
-sudo chmod 644 /etc/systemd/system/upaste.service
-sudo systemctl daemon-reload
-sudo systemctl enable upaste
-sudo systemctl restart upaste
+function deploy_service {
+    echo "*** deploying $1..."
+    set -x
+    sudo cp systemd/$1.service /etc/systemd/system/$1.service
+    sudo chmod 644 /etc/systemd/system/$1.service
+    sudo systemctl daemon-reload
+    sudo systemctl enable $1
+    sudo systemctl restart $1
+    set +x
+}
 
-sudo cp systemd/badge.service /etc/systemd/system/badge.service
-sudo chmod 644 /etc/systemd/system/badge.service
-sudo systemctl daemon-reload
-sudo systemctl enable badge
-sudo systemctl restart badge
+if $ALL || $ALL_SVCS || [[ "$SVC" = "upaste" ]]; then
+    deploy_service "upaste"
+fi
 
-./logs.sh
+if $ALL || $ALL_SVCS || [[ "$SVC" = "badge" ]]; then
+    deploy_service "badge"
+fi
+
+if $ALL || $ALL_SVCS || [[ "$SVC" = "homepage" ]]; then
+    deploy_service "homepage"
+fi
+
+if $TAIL; then
+    ./logs.sh
+fi
--- logs.sh
+++ logs.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+
 if [[ "$1" = "upaste" ]]; then
     sudo journalctl -fu upaste
+elif [[ "$1" = "badge" ]]; then
+    sudo journalctl -fu badge
+elif [[ "$1" = "homepage" ]]; then
+    sudo journalctl -fu homepage
+elif [[ "$1" = "all" ]]; then
+    sudo journalctl -f -u upaste -u badge -u homepage
 else
     sudo tail -f /var/log/nginx/access.log
 fi
